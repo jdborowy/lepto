83bfec7a447837269f7c09bd8f1531bf04263e1e
Jean-Daniel Borowy <jeandaniel.borowy@gmail.com>
2019-10-03 08:46:37 -0400

    Add Fred connector

--- pluto/data/connector/base.py
+++ pluto/data/connector/base.py
@@ -12,22 +12,33 @@ class Connector(abc.ABC):
         """fetch data"""
 
 
-class KrakenOHLC(Connector):
+class KrakenConnector(Connector):
 
     def __init__(self):
         self.columns = ["time", "open", "high", "low", "close", "vwap",
                         "volume", "count"]
         self.intervals = {"B": 1440, "D": 1440}
 
-    def _fetch_raw(self, ticker: str, freq=None):
-        url = f"https://api.kraken.com/0/public/OHLC?pair={ticker}"\
-              f"&interval={self.intervals.get(freq, 1440)}"
-        return urlrequest.urlopen(url).read()
+    def _build_url(self, ticker, freq=None):
+        return f"https://api.kraken.com/0/public/OHLC?pair={ticker}"\
+               f"&interval={self.intervals.get(freq, 1440)}"
 
-    def fetch(self, ticker: str, freq=None, start=None) -> pd.DataFrame:
+    def _fetch_raw(self, ticker, freq=None):
+        return urlrequest.urlopen(self._build_url(ticker, freq)).read()
+
+    def fetch(self, ticker, freq=None, start=None) -> pd.DataFrame:
         data_dict = json.loads(self._fetch_raw(ticker, freq))
         df = pd.DataFrame(data_dict["result"][ticker], columns=self.columns)
         df.set_index("time", inplace=True)
-        df = df.astype(float)
         df.index = pd.to_datetime(df.index, unit="s")
-        return df
+        return df.astype(float)
+
+
+class FredConnector(Connector):
+
+    def _build_url(self, ticker):
+        return f"https://fred.stlouisfed.org/graph/fredgraph.csv?id={ticker}"
+
+    def fetch(self, ticker, freq=None, start=None) -> pd.DataFrame:
+        return pd.read_csv(self._build_url(ticker),
+                           index_col=0, parse_dates=True)
--- pluto/data/connector/tests/test_base.py
+++ pluto/data/connector/tests/test_base.py
@@ -2,7 +2,7 @@ from unittest import TestCase, mock
 from os import path
 import pandas as pd
 
-from ..base import KrakenOHLC
+from ..base import KrakenConnector, FredConnector
 
 DIRNAME = path.dirname(path.abspath(__file__))
 
@@ -13,12 +13,40 @@ def _read_file(ticker, _):
         return f.read()
 
 
-class KrakenOHLCTest(TestCase):
+def _get_reference_df(ticker):
+    return pd.read_csv(path.join(DIRNAME, f"{ticker}_ref.csv"), index_col=0,
+                       parse_dates=True)
+
+
+class KrakenConnectorTest(TestCase):
+
+    def setUp(self):
+        self.connector = KrakenConnector()
+
+    def test_build_url(self):
+        expected = "https://api.kraken.com/0/public/OHLC?pair=XXBTZUSD" \
+                   "&interval=1440"
+        assert expected == self.connector._build_url("XXBTZUSD", "B")
 
     def test_fetch(self):
-        kraken_connector = KrakenOHLC()
-        kraken_connector._fetch_raw = mock.MagicMock(side_effect=_read_file)
-        ref = pd.read_csv(path.join(DIRNAME, "XXBTZUSD_ref.csv"), index_col=0,
-                          parse_dates=True)
-        result = kraken_connector.fetch("XXBTZUSD")
+        self.connector._fetch_raw = mock.MagicMock(side_effect=_read_file)
+        ref = _get_reference_df("XXBTZUSD")
+        result = self.connector.fetch("XXBTZUSD")
         pd.testing.assert_frame_equal(ref.astype(float), result)
+
+
+class FredConnectorTest(TestCase):
+
+    def setUp(self):
+        self.connector = FredConnector()
+
+    def test_build_url(self):
+        expected = "https://fred.stlouisfed.org/graph/fredgraph.csv?id=ICSA"
+        assert expected == self.connector._build_url("ICSA")
+
+    def test_fetch(self):
+        url = path.join(DIRNAME, "ICSA.csv")
+        self.connector._build_url = mock.MagicMock(return_value=url)
+        ref = _get_reference_df("ICSA")
+        result = self.connector.fetch("ICSA")
+        pd.testing.assert_frame_equal(ref, result)
